1e2eca82b50dbe5cb61d2cb3110ef38398646ef7
Andre <marques.andre.lm@gmail.com>
2020-01-20 16:57:44 +0000

    Added marker

--- screens/home/home.tsx
+++ screens/home/home.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { SafeAreaView } from "react-navigation";
 import Header from "../../shared/components/header/header";
-import {
-  AsyncStorage
-} from "react-native";
+import { AsyncStorage, Image } from "react-native";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
-import MapView from 'react-native-maps';
+import MapView, { MapEvent, Marker } from "react-native-maps";
 import WelcomeNotificationComponent from "../../shared/components/welcome/welcome";
-
 interface Props {
   navigation: any;
 }
 
-export default class HomePage extends React.Component<Props> {  
+export default class HomePage extends React.Component<Props> {
   state = {
     region: null,
     locationName: null,
-    showWelcome: true
-  }
-  
+    showWelcome: true,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+    markers: []
+  };
+
   private async getLocation() {
     const response: any = await Permissions.askAsync(Permissions.LOCATION);
-    if (response.granted !== 'granted' && response.granted) {
+    if (response.granted !== "granted" && response.granted) {
       return false;
     }
 
-    var provider = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.High});
-    
+    var provider = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High
+    });
+
     this.setMap(provider);
 
     await this.storeLocation(provider);
@@ -51,9 +53,16 @@ export default class HomePage extends React.Component<Props> {
       latitude: region.coords.latitude,
       longitude: region.coords.longitude
     });
-    this.setState({
-      locationName: name[0].city + ',' + name[0].country
-    })
+
+    if (name.length > 0) {
+      this.setState({
+        locationName: name[0].city + "," + name[0].country
+      });
+    } else {
+      this.setState({
+        locationName: 'Define location'
+      });
+    }
   }
 
   async componentWillMount() {
@@ -69,7 +78,10 @@ export default class HomePage extends React.Component<Props> {
   render() {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-        <Header locationName={this.state.locationName} navigation={this.props.navigation}></Header>
+        <Header
+          locationName={this.state.locationName}
+          navigation={this.props.navigation}
+        ></Header>
         {this.showMap()}
         {this.showWelcomeMessage()}
       </SafeAreaView>
@@ -79,36 +91,66 @@ export default class HomePage extends React.Component<Props> {
   showWelcomeMessage() {
     if (this.state.showWelcome) {
       return (
-        <WelcomeNotificationComponent onClose={() => this.setState({showWelcome: false})}></WelcomeNotificationComponent>
+        <WelcomeNotificationComponent
+          onClose={() => this.setState({ showWelcome: false })}
+        ></WelcomeNotificationComponent>
       );
     }
   }
 
   showMap() {
     if (this.state.region !== null) {
-    return (
+      return (
         <MapView
-          region={{
+          initialRegion={{
             latitude: this.state.region.coords.latitude,
             longitude: this.state.region.coords.longitude,
-            latitudeDelta: 0,
-            longitudeDelta: 0
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta
           }}
           rotateEnabled={false}
-          style={{flex: 1}}
-        ></MapView>
-    );
+          style={{ flex: 1 }}
+          onPress={(location: MapEvent) => this.onMapPress(location)}
+        >
+          {this.showMarkers()}
+        </MapView>
+      );
     } else {
       <MapView
-          region={{
-            latitude: 37.421998333333335,
-            longitude: -122.08400000000002,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-          rotateEnabled={false}
-          style={{flex: 1}}
-        ></MapView>
+        initialRegion={{
+          latitude: 37.421998333333335,
+          longitude: -122.08400000000002,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        rotateEnabled={false}
+        style={{ flex: 1 }}
+      ></MapView>;
     }
+  }
+
+  showMarkers() {
+    return this.state.markers.map((marker, index) => {
+      return (
+        <Marker key={index} coordinate={marker.coordinate} onPress={() => this.onMarkerPress(marker.coordinate)}>
+          <Image source={require('../../shared/assets/marker.png')} />
+        </Marker>
+      );
+    });
+  }
+
+  onMapPress(location: MapEvent) {
+    this.setState({
+      markers: this.state.markers.concat({
+        coordinate: {
+          latitude: Number(location.nativeEvent.coordinate.latitude),
+          longitude: Number(location.nativeEvent.coordinate.longitude)
+        }
+      })
+    });
+  }
+
+  onMarkerPress(location) {
+    console.log(location.latitude);
   }
 }
--- screens/home/styles.tsx
+++ screens/home/styles.tsx
@@ -1,5 +1,23 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    
+    marker: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: "rgba(130,4,150, 0.9)",
+      },
+      markerWrap: {
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      ring: {
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+        backgroundColor: "rgba(130,4,150, 0.3)",
+        position: "absolute",
+        borderWidth: 1,
+        borderColor: "rgba(130,4,150, 0.5)",
+      }
 });
