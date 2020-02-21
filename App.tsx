1a947ef5d4ab6928087263823e9c5027a8bfc9ae
Andre <marques.andre.lm@gmail.com>
2020-02-21 16:58:18 +0000

    Custom drawer test

--- App.tsx
+++ App.tsx
@@ -1,23 +1,59 @@
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  SafeAreaView
+} from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IntroPage from "./screens/intro/intro";
 import LoginPage from "./screens/login/login";
 import RegisterPage from "./screens/register/register";
 import HomePage from "./screens/home/home";
 import DependenciesLoader from "./screens/loader/dependencies";
-import { createDrawerNavigator } from "react-navigation-drawer";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
+import { ScrollView } from "react-native-gesture-handler";
+import React, { Component } from "react";
+import { View, Text } from "native-base";
+import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
 
-const AppStack = createDrawerNavigator({
-  Home: {
-    screen: HomePage
+const CustomDrawerComponent = props => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#5A646B" }}>
+      <ScrollView>
+        <DrawerItems
+          {...props}
+          getLabel={scene => (
+            <View style={{
+              marginTop: heightPercentageToDP('7%'),
+              paddingHorizontal: widthPercentageToDP('5%'),
+              alignSelf: 'center'
+              }}>
+              <Text style={{
+                fontFamily: 'quicksand-bold',
+                fontSize: widthPercentageToDP('3.7%'),
+                color: 'white'
+              }}>{props.getLabel(scene)}</Text>
+            </View>
+          )}
+        ></DrawerItems>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const AppStack = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomePage
+    },
+    Intro: {
+      screen: IntroPage
+    }
   },
-  Intro: {
-    screen: IntroPage
-  },
-},
-{
-  initialRouteName: 'Home'
-});
+  {
+    initialRouteName: "Home",
+    contentComponent: CustomDrawerComponent
+  }
+);
 
 const AuthStack = createStackNavigator({
   Intro: {
